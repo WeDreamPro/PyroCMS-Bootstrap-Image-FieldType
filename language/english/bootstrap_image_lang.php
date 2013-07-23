b0dd018f5ee35b4c694776a8cc6d59b4fc78598d
jose fonseca <jose@ditecnologia.com>
2013-07-23 16:53:08 -0500

    Adding English to lang.

---
+++ language/english/bootstrap_image_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams:bootstrap_image.name'] = 'Image (Bootstrap Fileupload)';
+$lang['streams:bootstrap_image.folder'] = 'Upload Folder';
+$lang['streams:bootstrap_image.resize_width'] = 'Resize width';
+$lang['streams:bootstrap_image.resize_height'] = 'Resize height';
+$lang['streams:bootstrap_image.allowed_types'] = 'Allowed types (Ex: jpg|png)';
+$lang['streams:bootstrap_image.image_errors'] = 'The following errors occurred uploading your file:';
+$lang['streams:bootstrap_image.need_folder'] = 'You must set up an upload folder before continue.';
+$lang['streams:bootstrap_image.keep_ratio'] = 'Keep ratio'; 
+$lang['streams:bootstrap_image.keep_ratio_instr'] = 'Keep ratio when resizing the image.';
+$lang['streams:bootstrap_image.allowed_types_instr'] = 'Example: jpg|jpeg|png';
+$lang['streams:bootstrap_image.select'] = 'Select image'; 
+$lang['streams:bootstrap_image.change'] = 'Change';
+$lang['streams:bootstrap_image.remove'] = 'Delete'; 
